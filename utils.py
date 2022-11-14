a421ebf299a3c1746c4b870c782087b802802edc
Mechrevo <dd101bb@qq.com>
2022-11-14 18:47:09 +0000

    formatted codes in utils.py

--- utils.py
+++ utils.py
@@ -8,14 +8,14 @@ def read_attendees_file(filepath: Path) -> CityCollection:
     return CityCollection(filepath)
 
 
-
-long = random.random() * 360. - 180.
-lati = random.random() * 180. - 90.
-attendees = random.randint(0, 500)
-h= City(name='a 6868 56 v bjk WEUIRYIWE', country='aaa', num_attendees=attendees, longitude=long, latitude=lati)
-
-
-
-city_collection_all = CityCollection(Path('attendee_locations.csv'))
-city_collection_all.summary(h)
-city_collection_all.plot_top_emitters(h,save=True)
+#
+# long = random.random() * 360. - 180.
+# lati = random.random() * 180. - 90.
+# attendees = random.randint(0, 500)
+# h= City(name='a 6868 56 v bjk WEUIRYIWE', country='aaa', num_attendees=attendees, longitude=long, latitude=lati)
+#
+#
+#
+# city_collection_all = CityCollection(Path('attendee_locations.csv'))
+# city_collection_all.summary(h)
+# city_collection_all.plot_top_emitters(h,save=True)
